c9081ee2f80fc601e87ff41d6cc1530ce8474701
Misael <misael.maldonado900@gmail.com>
2024-03-12 14:48:53 -0300

    Repo

---
+++ Ejercicio Katalon (Mobile f)/TestApkn11/Scripts/TestTrianguloEqui/Script1709926113603.groovy
@@ -0,0 +1,37 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\Users\\Usuario\\Desktop\\Curso Testing Master QA\\UTN QA profesional\\Modulo 2\\Unidad 1\\TestingAvanzadoApp11.apk', 
+    true)
+
+Mobile.tap(findTestObject('Object Repository/ObjTestTriangulo/android.widget.Button - Triangulo'), 0)
+
+Mobile.setText(findTestObject('Object Repository/ObjTestTriangulo/android.widget.EditText'), '1', 0)
+
+Mobile.setText(findTestObject('Object Repository/ObjTestTriangulo/android.widget.EditText (1)'), '1', 0)
+
+Mobile.setText(findTestObject('Object Repository/ObjTestTriangulo/android.widget.EditText (2)'), '1', 0)
+
+Mobile.tap(findTestObject('Object Repository/ObjTestTriangulo/android.widget.Button - Calcular'), 0)
+
+Mobile.verifyElementText(findTestObject('Object Repository/ObjTestTriangulo/android.widget.EditText - S, es un tringulo'), 
+    'Sí, es un triángulo')
+
+Mobile.closeApplication()
+
---
+++ Ejercicio Katalon (Mobile f)/TestApkn11/Test Suites/Tennis.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Tennis</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>91116d97-471f-46b1-a415-f021fdb6116f</testSuiteGuid>
+   <testCaseLink>
+      <guid>b8c48cbe-4283-49d7-b02a-ed987cf132ee</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TenisGameCompleto</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a629c94e-b319-4417-8841-6e670c2de79a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TenisPartidaCompleta</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
